3b10239c7346e5aed33e647d8426b0af7f593019
Alberto López Sánchez <albertolopezsanchez9@gmail.com>
2016-01-27 19:15:06 +0100

    Added calculus to closest point to bezier curve and fix minor errors

--- script.js
+++ script.js
@@ -7,14 +7,11 @@ $(document).ready(function() {
     var curves = [];
     var points = [];
     var index = 0;
+    var mousePos = {x:0,y:0};
+
     // Catch click event to create lines into canvas
     $('#mycanvas').click(function(event) {
-        var mouseX = event.pageX - $(this).offset().left;
-        var mouseY = event.pageY - $(this).offset().top;
-        points[index] = {
-            x: mouseX,
-            y: mouseY
-        };
+        points[index] = mousePos;
         index = (index + 1) % 4;
     });
 
@@ -26,14 +23,28 @@ $(document).ready(function() {
         }
     });
 
+    canvas.addEventListener('mousemove', function(evt) {
+        mousePos = getMousePos(canvas, evt);
+    }, false);
+
+
     function update() {
+        //clean canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        //draw bezier curves
         curves.forEach(function(elem) {
             drawBezier(ctx, elem.points);
         });
+        //draw points to make a new bezier curve
         points.forEach(function(elem) {
             drawCircle(ctx, elem, 3);
         });
+        //draw mouse point and the closest point to all of curves
+        drawCircle(ctx, mousePos, 3);
+        curves.forEach(function(elem) {
+            drawDistance(ctx, elem, mousePos);
+        });
+
     }
 
     window.setInterval(update, 1000 / 60);
@@ -41,6 +52,38 @@ $(document).ready(function() {
 
 
 
+function drawDistance(ctx, curve, point) {
+    var curvePoint = projection(curve, point);
+    ctx.strokeStyle = "pink";
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    ctx.moveTo(point.x, point.y);
+    ctx.lineTo(curvePoint.x, curvePoint.y);
+    ctx.stroke();
+    drawCircle(ctx, curvePoint, 3);
+}
+
+function projection(curve, point) {
+    var luts = curve.getLUT();
+    var i,
+        end = luts.length,
+        dist,
+        minDist = distance(luts[0], point),
+        sec = 0;
+    for (i = 1; i < end; i++) {
+        dist = distance(luts[i], point);
+        if (dist < minDist) {
+            sec = i;
+            minDist = dist;
+        }
+    }
+    var t = sec / (end - 1);
+    return curve.get(t);
+}
+
+function distance(p, q) {
+    return Math.abs(Math.sqrt(Math.pow((p.x - q.x), 2) + Math.pow((p.y - q.y), 2)));
+}
 
 function drawCircle(ctx, center, radius) {
 
@@ -63,6 +106,7 @@ function drawBezier(ctx, data) {
         points.push(elem.y);
     });
     ctx.lineWidth = 5;
+    ctx.strokeStyle = 'gray';
     ctx.beginPath();
     ctx.moveTo(points[0], points[1]);
 
@@ -78,8 +122,17 @@ function drawBezier(ctx, data) {
 function getRandomColor() {
     var letters = '0123456789ABCDEF'.split('');
     var color = '#';
-    for (var i = 0; i < 6; i++ ) {
+    for (var i = 0; i < 6; i++) {
         color += letters[Math.round(Math.random() * 15)];
     }
     return color;
+}
+
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
 }
